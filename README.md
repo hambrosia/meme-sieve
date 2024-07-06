b9f752f1b1f3628925b55c7628797d6a317295a7
Matt Hambro <matthew.hambro@gmail.com>
2024-07-05 17:12:00 -0700

    feat: ready for CLI usage with other Unix-style programs, update README

--- README.md
+++ README.md
@@ -1,2 +1,58 @@
 # meme-sieve
-Filter memes from your photo library
+Filter memes from your photo library.
+
+
+## Usage
+
+
+### API Setup
+Set the Google API Key environment variable.
+```shell
+$ export GOOGLE_API_KEY="gabagool"
+```
+
+
+### Running
+Run the program and it will print the names of files in the current folder that it identifies as memes.
+
+```shell 
+$ ../main.py 
+beans.jpg
+pizza_roll.jpg
+simpsons.jpg
+cat_comic.png
+idgaf.png
+```
+
+To move or copy the memes out of the directory, pipe the results to the desired command.
+
+
+### Copy
+To copy the memes to another foloder, use the following:
+
+```shell
+./main.py | while read filename; do cp "$filename" "../output_folder/$filename"; done
+```
+
+
+### Move
+To move the memes to another folder, use the following:
+```shell
+./main.py | while read filename; do cp "$filename" "../output_folder/$filename"; done
+```
+
+Always exercise caution, as the program can sometimes misidentify photos as memes, especially if they are humorous or contain meme-like text.
+
+
+### Setting the delay
+
+The model used is Google's Gemini Flash, which has a free-tier limit of 15 requests per minute. By default, the program will use a delay of four seconds to keep usage within the free tier. If you have a paid token, you can decrease the limit using the `-d` flag.
+
+```shell
+$ ./main.py -d 2
+beans.jpg
+pizza_roll.jpg
+simpsons.jpg
+cat_comic.png
+idgaf.png
+```
--- main.py
+++ main.py
@@ -4,9 +4,11 @@ import google.generativeai as genai
 import os
 from glob import glob
 import time
-from IPython.display import Image
+from PIL import Image
+import argparse
+from pathlib import Path
+import sys
 
-genai.configure(api_key=os.environ["GOOGLE_API_KEY"])
 
 
 PROMPT = """You are a gen z heavy instagram user. You know memes like the back of your hand. Your job is to label an image as a meme: true / false.
@@ -21,7 +23,7 @@ def generate_text_using_image(prompt, image_path, sleep_time=4):
     start = time.perf_counter()
     model = genai.GenerativeModel('gemini-1.5-flash-latest', system_instruction=prompt)
 
-    response = model.generate_content(contents=[Image(image_path)])
+    response = model.generate_content(contents=[Image.open(image_path)])
 
     end = time.perf_counter()
     duration = end - start
@@ -30,13 +32,23 @@ def generate_text_using_image(prompt, image_path, sleep_time=4):
 
 
 if __name__ == "__main__":
-    folder = input("Enter folder name to check for memes. Must be in same folder as script\n")
+    parser = argparse.ArgumentParser(prog="Meme Sieve", description="Returns a list of filepaths for memes in the specified folder")
+    parser.add_argument("-s", "--source_folder", default=".", type=str, help="The source folder path containing the files to be checked for memes")
+    parser.add_argument("-d", "--delay", default=4, type=int, help="The amount of seconds to wait between each examined file. This helps the tool stay within thre free tier for the Gemini Flash model")
 
-    file_paths = glob(f"{folder}/*")
-    print("Files found:\n")
-    meme_index = {}
+    args = parser.parse_args()
+    file_paths = glob(f"{args.source_folder}/*")
+
+    try:
+        genai.configure(api_key=os.environ["GOOGLE_API_KEY"])
+    except Exception:
+        sys.exit("Error: GOOGLE_API_KEY env var must be set with a valid Google API key.")
+        
+    
     for path in file_paths:
-        res_text = generate_text_using_image(prompt=PROMPT, image_path=path)
-        print(f"{path}\n{res_text}")
-        meme_index[path] = True if "true" in res_text else False
-    print(meme_index)    
+        res_text = generate_text_using_image(prompt=PROMPT, image_path=path, sleep_time=args.delay)
+        if "true" in res_text:
+            if args.source_folder == ".":
+                print(Path(path).name)
+            else:
+                print(path)
